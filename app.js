7a11b512de0459108c7b1afcbbe120ba9628f7a7
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-10-03 22:31:43 +0530

    Intial setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
