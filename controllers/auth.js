2afaf2006e9a8feb705ec77c7d9c4dae227fefcc
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-11-18 00:34:43 +0530

    login api added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
-const loginUser = (req, res, next) => {};
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) return res.status(404).json({ msg: "User doesn't exist !!" });
+  const isPasswordCorrect = await bcrypt.compareSync(password, user.password);
+  if (!isPasswordCorrect)
+    return res.status(401).json({ msg: "Password is not correct !!" });
+  return res.status(200).json({ token: generateAuthToken(user) });
+};
 
 const createUser = async (req, res, next) => {
   const { name, password, phone_no, email, username } = req.body;
@@ -16,11 +27,20 @@ const createUser = async (req, res, next) => {
       password: hashedPassword,
     });
     await user.save();
-    return res.status(200).json({ user });
+    return res.status(200).json({ token: generateAuthToken(user) });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ msg: "Something went wrong !!" });
   }
+};
+
+const validateUserLoginData = (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username)
+    return res.status(400).json({ msg: "Username is not present !!" });
+  if (!password)
+    return res.status(400).json({ msg: "Password is not present !!" });
+  next();
 };
 
 const validateUserSignUpData = (req, res, next) => {
@@ -53,9 +73,16 @@ const checkUserExists = async (req, res, next) => {
   }
   next();
 };
+
+const generateAuthToken = (user) => {
+  const token = jwt.sign(user.toObject(), process.env.JWT_SECRET);
+  return token;
+};
+
 module.exports = {
   loginUser,
   createUser,
   validateUserSignUpData,
   checkUserExists,
+  validateUserLoginData,
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,13 @@ const {
   createUser,
   validateUserSignUpData,
   checkUserExists,
+  validateUserLoginData,
 } = require("../controllers/auth");
 const { ROUTE_PATHS } = require("../utils/constants");
 
 const router = require("express").Router();
 
-router.post(ROUTE_PATHS.LOGIN, loginUser);
+router.post(ROUTE_PATHS.LOGIN, validateUserLoginData, loginUser);
 router.post(
   ROUTE_PATHS.SIGNUP,
   validateUserSignUpData,
