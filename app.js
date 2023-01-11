d35d1d1f38acf12d60d11f1335b7f5bf8a58fa5d
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-01-11 19:05:54 +0530

    authenticate user middleware added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const connectDB = require("./db/connect");
 const authRoutes = require("./routes/authRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const { ROUTE_PATHS } = require("./utils/constants");
+const { authenticateUser } = require("./controllers/utils");
 const app = express();
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -13,7 +14,7 @@ app.use(express.json());
 
 // routes
 app.use(ROUTE_PATHS.AUTH, authRoutes);
-app.use(ROUTE_PATHS.CHAT, chatRoutes);
+app.use(ROUTE_PATHS.CHAT, authenticateUser, chatRoutes);
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
---
+++ controllers/utils.js
@@ -0,0 +1,22 @@
+const jwtDecode = require("jwt-decode");
+const authenticateUser = async (req, res, next) => {
+  const headers = req.headers;
+  if (!headers.authorization)
+    return res.status(401).json({ msg: "Unauthorized" });
+  try {
+    const token = headers.authorization.split(" ")[1];
+    if (token) {
+      const user = await jwtDecode(token);
+      req.body.userID = user._id;
+      next();
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+  return res.status(401).json({ msg: "Unauthorized" });
+};
+
+module.exports = {
+  authenticateUser,
+};
